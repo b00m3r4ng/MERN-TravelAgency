95e7297cc8ad0e85a44971631d048f3ec7f16302
b00m3r4ng <bfrisby1992@gmail.com>
2025-01-09 13:53:11 -0500

    Added search bar and some CSS

--- tour-management/frontend/src/pages/Home.jsx
+++ tour-management/frontend/src/pages/Home.jsx
@@ -7,47 +7,61 @@ import heroImg02 from "../assets/images/hero-img02.jpg";
 import heroVideo from "../assets/images/hero-video.mp4";
 import worldImg from "../assets/images/world.png";
 import Subtitle from "../shared/Subtitle.jsx";
+import SearchBar from "../shared/SearchBar.jsx";
 
 const Home = () => {
   return (
     <>
-      <Container>
-        <Row>
-          <Col lg="6">
-            <div className="hero__content">
-              <div className="hero__subtitle d-flex align-items-center">
-                <Subtitle subtitle={"Know Before You Go"} />
-                <img src={worldImg} alt="" />
+      <section>
+        <Container>
+          <Row>
+            <Col lg="6">
+              <div className="hero__content">
+                <div className="hero__subtitle d-flex align-items-center">
+                  <Subtitle subtitle={"Know Before You Go"} />
+                  <img src={worldImg} alt="" />
+                </div>
+                <h1>
+                  Traveling opens the door to creating{" "}
+                  <span className="highlight">memories</span>
+                </h1>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. In
+                  commodi distinctio expedita nostrum officia repellendus,
+                  cumque corrupti aspernatur illum culpa molestiae enim tempore
+                  numquam explicabo voluptas quis iure earum inventore.
+                </p>
               </div>
-              <h1>
-                Traveling opens the door to creating{" "}
-                <span className="highlight">memories</span>
-              </h1>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. In
-                commodi distinctio expedita nostrum officia repellendus, cumque
-                corrupti aspernatur illum culpa molestiae enim tempore numquam
-                explicabo voluptas quis iure earum inventore.
-              </p>
-            </div>
-          </Col>
-          <Col lg="2">
-            <div className="hero__img-box">
-              <img src={heroImg} alt="" />
-            </div>
-          </Col>
-          <Col lg="2">
-            <div className="hero__img-box mt-4">
-              <video src={heroVideo} alt="" controls />
-            </div>
-          </Col>
-          <Col lg="2">
-            <div className="hero__img-box mt-5">
-              <img src={heroImg02} alt="" />
-            </div>
-          </Col>
-        </Row>
-      </Container>
+            </Col>
+            <Col lg="2">
+              <div className="hero__img-box">
+                <img src={heroImg} alt="" />
+              </div>
+            </Col>
+            <Col lg="2">
+              <div className="hero__img-box mt-4">
+                <video src={heroVideo} alt="" controls />
+              </div>
+            </Col>
+            <Col lg="2">
+              <div className="hero__img-box mt-5">
+                <img src={heroImg02} alt="" />
+              </div>
+            </Col>
+            <SearchBar />
+          </Row>
+        </Container>
+      </section>
+      <section>
+        <Container>
+          <Row>
+            <Col lg="3">
+              <h5 className="services__subtitle">What we serve</h5>
+              <h2 className="services__title">We offer out best services</h2>
+            </Col>
+          </Row>
+        </Container>
+      </section>
     </>
   );
 };
